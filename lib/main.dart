0cebbaad39946c4ea2cbb34b05bbe52d1b0e7222
Daniel Augusto Garrido <DaniielGarrido@gmail.com>
2023-06-13 22:27:05 -0600

    Second Widget Cosumiendo Secon en Fisth

--- lib/main.dart
+++ lib/main.dart
@@ -22,26 +22,7 @@ class Booksy extends StatelessWidget {
                 width: 200,
               ),
             ),
-            Padding(padding: EdgeInsets.only(top: 40)),
-            Text(
-              "The Hitchhiker's Guide to the Galaxy",
-              textDirection: TextDirection.ltr,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.black87, fontSize: 30),
-            ),
-            Padding(padding: EdgeInsets.only(top: 15)),
-            Text(
-              "Douglas Adams",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(color: Colors.black54, fontSize: 20),
-            ),
-            Padding(padding: EdgeInsets.only(top: 10)),
-            Text(
-              "It’s an ordinary Thursday morning for Arthur Dent . . . until his house gets demolished. The Earth follows shortly after to make way for a new hyperspace express route, and Arthur’s best friend has just announced that he’s an alien.",
-              textDirection: TextDirection.ltr,
-              textAlign: TextAlign.justify,
-              style: TextStyle(color: Colors.black54, fontSize: 15),
-            ),
+            BookDescription(),
             Padding(padding: EdgeInsets.only(bottom: 200)),
             Directionality(
                 textDirection: TextDirection.ltr,
@@ -55,6 +36,36 @@ class Booksy extends StatelessWidget {
         ),
       ),
       color: Colors.white,
+    );
+  }
+}
+
+class BookDescription extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(padding: EdgeInsets.only(top: 40)),
+        Text(
+          "The Hitchhiker's Guide to the Galaxy",
+          textDirection: TextDirection.ltr,
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.black87, fontSize: 30),
+        ),
+        Padding(padding: EdgeInsets.only(top: 15)),
+        Text(
+          "Douglas Adams",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.black54, fontSize: 20),
+        ),
+        Padding(padding: EdgeInsets.only(top: 10)),
+        Text(
+          "It’s an ordinary Thursday morning for Arthur Dent . . . until his house gets demolished. The Earth follows shortly after to make way for a new hyperspace express route, and Arthur’s best friend has just announced that he’s an alien.",
+          textDirection: TextDirection.ltr,
+          textAlign: TextAlign.justify,
+          style: TextStyle(color: Colors.black54, fontSize: 15),
+        ),
+      ],
     );
   }
 }
